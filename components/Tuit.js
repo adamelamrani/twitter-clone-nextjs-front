5b3cbe03f189e16525cf0fa3dfedff49d08568c7
Adam El Amrani <adelamco1091@outlook.com>
2022-03-02 19:42:01 +0100

    Created tuit form component

---
+++ components/Tuit.js
@@ -0,0 +1,24 @@
+import { useState } from "react/cjs/react.production.min"
+
+const Tuit = () => {
+  const blankTuit = "";
+
+  const [tuit, setTuit] = useState(blankTuit);
+
+  const tuitInput = (event) => {
+    setTuit({
+      ...tuit, 
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <form onSubmit={}>
+      <label htmlFor="tuit-box">Writte your tuit</label>
+      <input type="text" onChange={tuitInput} maxLength={200} placeholder="Say something..."/>
+      <button type="submit">Tuit</button>
+    </form>
+  )
+}
+
+export default Tuit;
