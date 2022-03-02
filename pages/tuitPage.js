b20f1d497b911326f968c6a1703ea1016724248c
Silvia Martinez <sil@silvia.com>
2022-03-03 00:17:56 +0100

    Delete console

--- pages/tuitPage.js
+++ pages/tuitPage.js
@@ -1,7 +1,6 @@
 import Tuit from "../components/Tuit";
 
 const tuitPage = ({ tuits }) => {
-  console.log(tuits);
   return (
     <>
       <h2>List of tuits</h2>
@@ -15,7 +14,7 @@ const tuitPage = ({ tuits }) => {
 export const getServerSideProps = async () => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_TUITAH_API}list`);
   const tuits = await response.json();
-  console.log(tuits);
+
   return {
     props: tuits,
   };
